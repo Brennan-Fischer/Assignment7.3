9127c10a75b6319c6d9d8640cfbbecadff110d8b
Brennan <blfische92@gmail.com>
2024-06-24 15:32:06 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using BinaryTree;
+
+namespace Assignment7._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int searchFor = 7;
+            var binaryTree = new BinaryTree<int>() { 8, 5, 12, 3, 7, 10, 15 };
+
+            if( binaryTree.Contains(searchFor) )
+            {
+                Console.WriteLine($"Tree contains {searchFor}");
+            }
+            else
+            {
+                Console.WriteLine($"Tree does not contain {searchFor}");
+            }
+        }
+    }
+}
